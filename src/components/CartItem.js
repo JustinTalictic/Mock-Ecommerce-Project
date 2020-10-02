9741ebcf3f0e3c54de7424f2def1524cc88b5c70
Justin Talictic <jaytalictic@gmail.com>
2020-10-02 15:16:13 -0500

    UX improvements. added custom hook

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,12 +1,22 @@
 import React, { useContext } from "react"
 import {Context} from "../Context"
+import useHover from "../hooks/useHover"
 
 function CartItem({item}) {
-const {removeFromCart} = useContext(Context)
+    const [hovered, ref] = useHover()
+    const {removeFromCart} = useContext(Context)
+
+    const iconClassName = hovered ? "ri-delete-bin-fill" : "ri-delete-bin-line"
 
     return (
         <div className="cart-item">
-            <i className="ri-delete-bin-line" onClick={() => removeFromCart(item.id)}></i>
+            <i 
+                className={iconClassName}
+                onClick={() => removeFromCart(item.id)}
+                ref={ref}
+            >
+            </i>
+
             <img src={item.url} width="130px" />
             <p>$5.99</p>
         </div>
